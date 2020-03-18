6e5ca59398f9c34ed94887d9e7064dfaf27e2d72
llARXANGELll <aramh@inbox.ru>
2020-03-18 15:46:00 +0300

    Все настрил вроде

--- src/test/java/work20/MainTest.java
+++ src/test/java/work20/MainTest.java
@@ -1,4 +1,35 @@
 package work20;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainTest {
+
+    private WebDriver webDriver;
+
+    @BeforeSuite
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        webDriver.get("https://idemo.bspb.ru/");
+    }
+
+    @Test(enabled = true)
+    public void work20() {
+        String attributeUsername = webDriver.findElement(By.xpath("//div/input[@value='demo']")).getAttribute("dem");
+        Assert.assertEquals(attributeUsername, "demo");
+    }
+    @AfterTest
+    public void driverOut() {
+        webDriver.quit();
+    }
 }
